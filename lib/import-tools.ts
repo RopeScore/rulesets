878586d1886b29f9eca765c40a0967b9f5962ff3
Svante Bengtson <svante@swantzter.se>
2023-08-30 21:15:12 +0200

    fix ts nodenext import

--- lib/import-tools.ts
+++ lib/import-tools.ts
@@ -10,7 +10,7 @@ const rulesetRegex = /^[a-z0-9-]+@(?<version>[a-z0-9-.]+)$/
 export async function importCompetitionEventModel (modelId: string): Promise<CompetitionEventModel> {
   if (!modelRegex.test(modelId)) throw new TypeError('Invalid competitionEvent provided must be in the form of <model id>@<version>')
   try {
-    return (await import(`./models/competition-events/${modelId}.js`)).default
+    return (await import(`./models/competition-events/${modelId}.js`)).default.default
   } catch {
     throw new RSUnsupported('preconfigured', modelId)
   }
@@ -18,7 +18,7 @@ export async function importCompetitionEventModel (modelId: string): Promise<Com
 export async function importOverallModel (modelId: string): Promise<OverallModel> {
   if (!modelRegex.test(modelId)) throw new TypeError('Invalid competitionEvent provided must be in the form of <model id>@<version>')
   try {
-    return (await import(`./models/overalls/${modelId}.js`)).default
+    return (await import(`./models/overalls/${modelId}.js`)).default.default
   } catch {
     throw new RSUnsupported('preconfigured', modelId)
   }
@@ -28,7 +28,7 @@ export async function importPreconfiguredCompetitionEvent (competitionEvent: str
   const match = evtDefRegex.exec(competitionEvent)
   if (match?.groups?.org == null || match.groups?.version == null) throw new TypeError('Invalid competitionEvent provided must be in the form of <event definition lookup code>@<version>')
   try {
-    return (await import(`./preconfigured/competition-events/${match.groups.org}/${match.groups.version}/${competitionEvent}.js`)).default
+    return (await import(`./preconfigured/competition-events/${match.groups.org}/${match.groups.version}/${competitionEvent}.js`)).default.default
   } catch {
     throw new RSUnsupported('preconfigured', competitionEvent)
   }
@@ -37,7 +37,7 @@ export async function importPreconfiguredOverall (competitionEvent: string): Pro
   const match = evtDefRegex.exec(competitionEvent)
   if (match?.groups?.org == null || match.groups?.version == null) throw new TypeError('Invalid competitionEvent provided must be in the form of <event definition lookup code>@<version>')
   try {
-    return (await import(`./preconfigured/overalls/${match.groups.org}/${match.groups.version}/${competitionEvent}.js`)).default
+    return (await import(`./preconfigured/overalls/${match.groups.org}/${match.groups.version}/${competitionEvent}.js`)).default.default
   } catch {
     throw new RSUnsupported('preconfigured', competitionEvent)
   }
@@ -47,7 +47,7 @@ export async function importRuleset (rulesetId: string): Promise<Ruleset> {
   if (!rulesetRegex.test(rulesetId)) throw new TypeError('Invalid competitionEvent provided must be in the form of <ruleset id>@<version>')
   if (rulesetId === 'types') throw new RSUnsupported('preconfigured', rulesetId)
   try {
-    return (await import(`./rulesets/${rulesetId}.js`)).default
+    return (await import(`./rulesets/${rulesetId}.js`)).default.default
   } catch {
     throw new RSUnsupported('preconfigured', rulesetId)
   }
