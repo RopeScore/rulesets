6810da4d26ba53b346090875610dbbb7eba5e801
Svante Bengtson <svante@swantzter.se>
2024-10-22 16:00:02 +0200

    WIP DD

--- lib/models/competition-events/ijru.freestyle.wh@4.0.0.ts
+++ lib/models/competition-events/ijru.freestyle.wh@4.0.0.ts
@@ -1,23 +1,16 @@
 import type { CompetitionEventModel } from '../types'
 import type { Option } from './ijru.freestyle.sr@4.0.0'
-import { default as SR, difficultyJudgeFactory, freestylePreviewTableHeaders, freestyleResultTableHeaders, presentationJudge, technicalJudge } from './ijru.freestyle.sr@4.0.0'
+import { default as SR, calculateEntryFactory, difficultyJudgeFactory, freestylePreviewTableHeadersFactory, freestyleResultTableHeaders, presentationJudge, technicalJudgeFactory } from './ijru.freestyle.sr@4.0.0'
 
 export default {
   id: 'ijru.freestyle.wh@4.0.0',
   name: 'IJRU Wheel Freestyle v4.0.0',
-  options: [
-    { id: 'discipline', name: 'Discipline', type: 'enum', enum: ['wh'] },
-    { id: 'interactions', name: 'Has Interactions', type: 'boolean' },
-    { id: 'maxRqGymnasticsPower', name: 'Power/Gymnastics Required Elements', type: 'number', min: 0, step: 1 },
-    { id: 'maxRqMultiples', name: 'Multiples Required Elements', type: 'number', min: 0, step: 1 },
-    { id: 'maxRqRopeManipulation', name: 'Rope Manipulation Required Elements', type: 'number', min: 0, step: 1 },
-    { id: 'maxRqInteractions', name: 'Interactions Required Elements', type: 'number', min: 0, step: 1 },
-  ],
-  judges: [presentationJudge, technicalJudge, difficultyJudgeFactory('Da', 'Difficulty - Athlete A'), difficultyJudgeFactory('Db', 'Difficulty - Athlete B')],
+  options: SR.options,
+  judges: [presentationJudge, technicalJudgeFactory({ discipline: 'sr' }), difficultyJudgeFactory('Da', 'Difficulty - Athlete A', { discipline: 'sr' }), difficultyJudgeFactory('Db', 'Difficulty - Athlete B', { discipline: 'sr' })],
 
-  calculateEntry: SR.calculateEntry,
+  calculateEntry: calculateEntryFactory({ discipline: 'wh' }),
   rankEntries: SR.rankEntries,
 
-  previewTable: options => freestylePreviewTableHeaders(options),
+  previewTable: options => freestylePreviewTableHeadersFactory({ discipline: 'sr' })(options),
   resultTable: options => freestyleResultTableHeaders,
 } satisfies CompetitionEventModel<Option>
